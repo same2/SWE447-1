eba5aa7085de2f879c06a17443fa38fa26a59f3f
dninh <35516115+dninh@users.noreply.github.com>
2018-01-25 09:26:56 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -13,12 +13,14 @@ function init() {
     gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
     
     cone = new Cone( gl, n );
-    
     render();
+    gl.viewport(0,0,canvas.width,canvas.height);
+    gl.drawArrays(gl.POINTS, 0, 4);
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
