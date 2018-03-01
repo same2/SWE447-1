1dcf22ebf8ff6e2c0f1a9b85f102ab6acbc6c164
dninh <35516115+dninh@users.noreply.github.com>
2018-03-01 14:57:48 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -13,11 +13,51 @@ function init() {
     gl.clearColor( 0.0, 1.0, 0.0, 1.0 );
     cone = new Cone(gl);
     render();
+    
+    canvas.onmousedown = handleMouseDown;
+    document.onmouseup = handleMouseUp;
+    document.onmousemove = handleMouseMove;
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     cone.render();
 }
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
 
+var moonRotationMatrix = mat4.create();
+mat4.identity(moonRotationMatrix);
+
+function handleMouseDown(event) {
+    mouseDown = true;
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+}
+
+function handleMouseUp(event) {
+    mouseDown = false;
+}
+
+function handleMouseMove(event) {
+    if (!mouseDown) {
+        return;
+    }
+    var newX = event.clientX;
+    var newY = event.clientY;
+
+    var deltaX = newX - lastMouseX;
+    var newRotationMatrix = mat4.create();
+    mat4.identity(newRotationMatrix);
+    mat4.rotate(newRotationMatrix, degToRad(deltaX / 10), [0, 1, 0]);
+
+    var deltaY = newY - lastMouseY;
+    mat4.rotate(newRotationMatrix, degToRad(deltaY / 10), [1, 0, 0]);
+
+    mat4.multiply(newRotationMatrix, moonRotationMatrix, moonRotationMatrix);
+
+    lastMouseX = newX
+    lastMouseY = newY;
+}
 window.onload = init;
