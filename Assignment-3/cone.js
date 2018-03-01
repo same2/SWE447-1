29aec162a32528671e9b876e09655e8ee65f06db
dninh <35516115+dninh@users.noreply.github.com>
2018-03-01 13:48:17 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -21,4 +21,48 @@ function render() {
     cone.render();
 }
 
+function keydown(event) {
+    state.ui.pressedKeys[event.keyCode] = true;
+}
+
+function keyup(event) {
+    state.ui.pressedKeys[event.keyCode] = false;
+}
+
+function mousedown(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var rect = event.target.getBoundingClientRect();
+    // If we're within the rectangle, mouse is down within canvas.
+    if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
+      state.ui.mouse.lastX = x;
+      state.ui.mouse.lastY = y;
+      state.ui.dragging = true;
+    }
+}
+
+function mouseup(event) {
+    state.ui.dragging = false;
+}
+
+function mousemove(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    if (state.ui.dragging) {
+      // The rotation speed factor
+      // dx and dy here are how for in the x or y direction the mouse moved
+      var factor = 10/state.canvas.height;
+      var dx = factor * (x - state.ui.mouse.lastX);
+      var dy = factor * (y - state.ui.mouse.lastY);
+
+      // update the latest angle
+      state.app.angle.x = state.app.angle.x + dy;
+      state.app.angle.y = state.app.angle.y + dx;
+    }
+    // update the last mouse position
+    state.ui.mouse.lastX = x;
+    state.ui.mouse.lastY = y;
+  }
+})(window || this);
+
 window.onload = init;
